7ad73264534b9f3ccb74a0ff07aba665e71958b2
Roman Kozyrev <r.prohor7@gmail.com>
2025-03-20 12:55:34 +0100

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { lazy, Suspense } from 'react';
-import LazyLoad from 'react-lazyload';
-
-const HeroSection = lazy(() => import('./sections/HeroSection'));
-const ServicesSection = lazy(() => import('./sections/ServicesSection'));
-const PortfolioSection = lazy(() => import('./sections/PortfolioSection'));
-const ContsctsSection = lazy(() => import('./sections/ContsctsSection'));
+import HeroSection from './sections/HeroSection';
+import ServicesSection from './sections/ServicesSection';
+import PortfolioSection from './sections/PortfolioSection';
+import ContsctsSection from './sections/ContsctsSection';
 
 const App = () => {
   return (
     <div className="container">
-      <Suspense fallback={'loading...'}>
-        <LazyLoad height={'100vh'} offset={-100}>
-          <HeroSection />
-        </LazyLoad>
-      </Suspense>
-      <Suspense fallback={'loading...'}>
-        <LazyLoad height={'100vh'} offset={-100}>
-          <ServicesSection />
-        </LazyLoad>
-      </Suspense>
-      <Suspense fallback={'loading...'}>
-        <LazyLoad height={'100vh'} offset={-100}>
-          <PortfolioSection />
-        </LazyLoad>
-      </Suspense>
-      <Suspense fallback={'loading...'}>
-        <LazyLoad height={'100vh'} offset={-100}>
-          <ContsctsSection />
-        </LazyLoad>
-      </Suspense>
+      <HeroSection />
+      <ServicesSection />
+      <PortfolioSection />
+      <ContsctsSection />
     </div>
   );
 };
